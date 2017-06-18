8cee528e869352f793db20fc74240cd2a2d1bbe9
timedroll <timedroll@DESKTOP-HB5CMC3>
2017-06-18 12:44:39 +0300

    Custom 404 error page added

---
+++ SocialNetwork.Mvc/Controllers/ErrorsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialNetwork.Mvc.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
